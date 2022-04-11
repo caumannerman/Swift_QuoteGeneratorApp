1279ad2384560c0aa0c0a643ad9ff3ca41f9d661
Caumannerman <uigohiiming2@gmail.com>
2022-04-11 20:18:06 +0900

    viewController 첫 Load시, "명언 생성"버튼을 누르지 않아도 명언을 생성하여 보여주도록 수정

--- QuotesGenerator/QuotesGenerator/ViewController.swift
+++ QuotesGenerator/QuotesGenerator/ViewController.swift
@@ -9,26 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var colorView: UIView!
     @IBOutlet weak var quoteLabel: UILabel!
     @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var generateButton: UIButton!
+    
     let quotes = [
         Quote(contents: "죽음을 두려워하는 나머지 삶을 시작조차 못하는 사람이 많다.", name: "벤다이크"),
         Quote(contents: "나는 나 자신을 빼 놓고는 모두 안다.", name: "비용"),
         Quote(contents: "편견이란 실효성이 없는 의견이다.", name: "암브로스 빌"),
         Quote(contents: "분노는 바보들의 가슴속에서만 살아간다.", name: "아인슈타인"),
-        Quote(contents: "몇 번이라도 좋다! 이끔찍한 생이여....다시!", name: "니체")
+        Quote(contents: "몇 번이라도 좋다! 이끔찍한 생이여....다시!", name: "니체"),
+        Quote(contents: "나는 IOS+ 풀스택 개발자다", name: "양준식")
     ]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    //앱 실행 후 곧바로 하나의 명언이 보이도록 action을 인위적으로 생성 
+    override func viewDidAppear(_ animated: Bool) {
+        generateButton.sendActions(for: .touchUpInside)
+    }
 
 
     @IBAction func tapQuoteGeneratorBtn(_ sender: Any) {
-        let random = Int(arc4random_uniform(5)) // 0~ 4사이의 난수 생성
+        let random = Int(arc4random_uniform(6)) // 0~ 4사이의 난수 생성
         let quote = quotes[random]
         self.quoteLabel.text = quote.contents
         self.nameLabel.text = quote.name
+        
+        
         
     }
 }
